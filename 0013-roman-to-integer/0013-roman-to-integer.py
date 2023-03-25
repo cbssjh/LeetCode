1a433ba822367eff1830c5dfb462c7a98bbd1f87
Junhyeok Seo(서준혁) <79079708+cbssjh@users.noreply.github.com>
2023-03-25 21:59:41 +0000

    Time: 57 ms (26.58%), Space: 13.8 MB (63.66%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,13 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        #dictionary 활용
+        roman = {'I':1, 'V': 5, 'X' : 10, 'L' : 50, 'C' : 100, 'D' : 500, 'M' : 1000}
+        answer = 0
+        
+        for i in range(len(s)-1):
+            if roman[s[i]] < roman[s[i+1]]:
+                answer -= roman[s[i]]
+            else:
+                answer += roman[s[i]]
+        return answer + roman[s[-1]]
+        
