5986421afec2ea8bd50d66ca0ccc2ac51897fcac
Junhyeok Seo(서준혁) <79079708+cbssjh@users.noreply.github.com>
2023-03-30 14:25:10 +0000

    Time: 51 ms (66.37%), Space: 14.7 MB (29.08%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -3,7 +3,7 @@ class Solution:
         low = 0
         high = len(nums)
         while low < high:
-            # '/'하나면 소수점까지 찍히니깐 주의
+            # '/'하나면 소수점까지 찍히니깐 주의!
             mid = low + (high - low)//2    
             if (nums[mid] >= target):
                 high = mid
