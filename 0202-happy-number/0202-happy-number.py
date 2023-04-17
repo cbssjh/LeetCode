fbd659535e80944d004415e7335191a6bc4e8e93
Junhyeok Seo(서준혁) <79079708+cbssjh@users.noreply.github.com>
2023-04-17 17:17:54 +0100

    Time: 33 ms (78.78%), Space: 13.8 MB (48.09%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        def square_sum(num):
+            # 자릿수 제곱의 합을 구하는 함수
+            sum = 0
+            while num > 0:
+                sum += (num % 10) ** 2
+                num //= 10
+            return sum
+        
+        p = n
+        q = square_sum(n)
+        # 1이 될때까지, 혹은 
+        while q != 1 and p != q:
+            p = square_sum(p)
+            q = square_sum(square_sum(q))
+        return q == 1
+        
