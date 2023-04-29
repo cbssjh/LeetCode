291cecd93ce0a704efe2c2c5a87d8c4e875be0b5
Junhyeok Seo(서준혁) <79079708+cbssjh@users.noreply.github.com>
2023-04-29 23:25:50 +0100

    Time: 64 ms (20.13%), Space: 41.9 MB (64.84%) - LeetHub

--- 2648-generate-fibonacci-sequence/2648-generate-fibonacci-sequence.js
+++ 2648-generate-fibonacci-sequence/2648-generate-fibonacci-sequence.js
@@ -2,11 +2,15 @@
  * @return {Generator<number>}
  */
 var fibGenerator = function*() {
-    let x = 0;
-    let y = 1;
+    var x = 0;
+    var y = 1;
+    // 무한루프
     while (true) {
         yield x;
-    [x, y] = [y, x + y];
+        // [x, y] = [y, x + y]
+        var temp = y;
+        y = x + y;
+        x = temp;
   }
 };
 
