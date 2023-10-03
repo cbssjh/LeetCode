b10c54087048e89c43d82d6fb54b48c713dbc260
Junhyeok Seo(서준혁) <79079708+cbssjh@users.noreply.github.com>
2023-10-03 23:20:58 +0900

    Time: 35 ms (76.80%), Space: 15.4 MB (68.15%) - LeetHub

---
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def reverseVowels(self, s):
+        vowels = {'a', 'e', 'i', 'o', 'u'}
+        s = list(s)
+        left_index = 0
+        right_index = len(s) - 1
+
+        while left_index < right_index:
+            if s[left_index].lower() not in vowels:
+                left_index += 1
+                continue
+
+            if s[right_index].lower() not in vowels:
+                right_index -= 1
+                continue
+
+            s[left_index], s[right_index] = s[right_index], s[left_index]
+            left_index += 1
+            right_index -= 1
+
+        return ''.join(s) # 리스트를 문자열로 다시 변환
+        
